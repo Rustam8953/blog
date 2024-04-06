999a8b6ea2ae2f09bfa2cedb9590a862944eaf6e
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-04-06 18:12:54 +0700

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Header} from './components/Header';
 import {Home} from './pages/Home';
 import {Routes, Route} from 'react-router-dom';
 import { PostPage } from './pages/PostPage';
+import { Login } from './pages/Log';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/posts/:id" element={<PostPage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Container>
     </>
